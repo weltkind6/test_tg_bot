e118168290f15da26da4ec38c6abd2daabc2d0e9
yaroslavtcevi <yaroslavzef@icloud.com>
2022-07-19 15:39:47 +0300

    feat : add web-bot with back button #2

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,8 +1,8 @@
 const { Telegraf } = require('telegraf')
 
-const TOKEN = '5465423691:AAEkvM_2SXMs3PaE1zGhBX9YNx7CPC3H6Vo'
+const TOKEN = '5310564820:AAHLX4O0Pwr3iPc0XW1PtVIf1dcF30rJqBo'
 const bot = new Telegraf(TOKEN)
-const webLink = 'https://62d66f658212e867471a6e24--polite-gaufre-b001c2.netlify.app/'
+const webLink = 'https://62d680ef0028f9744a9a63e3--venerable-kelpie-e68d2c.netlify.app/'
 
 bot.start((ctx) => ctx.reply('Welcome my friend', {
     reply_markup: {keyboard: [[{text: 'web app', web_app: {url: webLink}}]]}
